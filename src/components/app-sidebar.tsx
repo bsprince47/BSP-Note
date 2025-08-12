776a7f30ff0e959cbc058c3a9c785e4df8bf84dd
bsprince47 <bsprince47@gmail.com>
2025-08-12 05:24:05 -0700

    css fix

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -125,7 +125,7 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>BSP Note 1.0.9</SidebarGroupLabel>
+          <SidebarGroupLabel>BSP Note 1.0.10</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
